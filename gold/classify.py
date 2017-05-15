40a4bfbf88e94f245f62c3327b328816ba8d5fd4
Pedro <alagoa.pedro@ua.pt>
2017-05-15 01:58:32 +0100

    classifying tool updated, some captures added

--- gold/classify.py
+++ gold/classify.py
@@ -6,6 +6,22 @@ import sys
 import warnings
 import scalogram
 
+
+def calcScalogram(data):
+	N=300
+	dj=1/64
+	s0=2
+	J=1/dj * np.log2(0.5*N/s0)
+	scales=s0*2**(np.arange(J)*dj)
+
+	allS=np.zeros((10,len(scales)))
+	for i in range(10):
+		S,scales=scalogram.scalogramCWT(data[:,i],scales)
+		allS[i,:]=S
+	
+	average=np.mean(allS,axis=0)
+	return average,scales
+
 def waitforEnter():
 	if sys.version_info[0] == 2:
 		raw_input("Press ENTER to continue.")
@@ -17,7 +33,6 @@ def curveDistance(c1, c2):
 
 
 def main():
-
 	########### Loading files and configuring ###########
 
 	plt.ion()
@@ -26,114 +41,66 @@ def main():
 	spotifydown=np.loadtxt('../data/spotifydown')
 	pornhubdown=np.loadtxt('../data/pornhubdown')
 	
-	test=np.loadtxt('../data/for_testing/spotify_test1')				# capture to test (can be more than one)
-	
-	N=len(youtubedown)
-	dj=1/64
-	s0=2
-	J=1/dj * np.log2(0.5*N/s0)
-	scales=s0*2**(np.arange(J)*dj)
+	test=np.loadtxt('../data/for_testing/youtube/youtube_test2')				# capture to test (can be more than one)
 
 	########### Profiling YouTube caps ###########
 	print("Profiling YouTube caps...")
-
-	allS=np.zeros((10,len(scales)))
-	for i in range(10):
-		S,scales=scalogram.scalogramCWT(youtubedown[:,i],scales)
-		allS[i,:]=S
-		#plt.plot(scales,S,'b')
-		#plt.show()
-		#idx=argrelextrema(S, np.greater)[0]
-		#threshold=.5*max(S)
-		#idxM=[j for j in idx if S[j]>threshold]
-		#print(scales[idxM])
-		#waitforEnter()
-	
-	averageS1=np.mean(allS,axis=0)
-	plt.plot(scales,averageS1,'r',lw=3)
+	scalogramAvgYoutube,scales = calcScalogram(youtubedown)
+	plt.plot(scales,scalogramAvgYoutube,'r',lw=3)
 	plt.show()
 	waitforEnter()
 
 	########### Profiling regular browsing caps ###########
 	print("Profiling regular browsing caps...")
-
-	allS=np.zeros((10,len(scales)))
-	for i in range(10):
-		S,scales=scalogram.scalogramCWT(browsingdown[:,i],scales)
-		allS[i,:]=S
-		#plt.plot(scales,S,'r')
-		#plt.show()
-		#idx=argrelextrema(S, np.greater)[0]
-		#threshold=.5*max(S)
-		#idxM=[j for j in idx if S[j]>threshold]
-		#print(scales[idxM])
-		#waitforEnter()
-	
-	averageS2=np.mean(allS,axis=0)
-	plt.plot(scales,averageS2,'b',lw=3)
+	scalogramAvgBrowsing, scales = calcScalogram(browsingdown)
+	plt.plot(scales,scalogramAvgBrowsing,'b',lw=3)
 	plt.show()
 	waitforEnter()
 
 	########### Profiling Spotify caps ###########
 	print("Profiling Spotify caps...")
-
-	allS=np.zeros((10,len(scales)))
-	for i in range(10):
-		S,scales=scalogram.scalogramCWT(spotifydown[:,i],scales)
-		allS[i,:]=S
-		#plt.plot(scales,S,'r')
-		#plt.show()
-		#idx=argrelextrema(S, np.greater)[0]
-		#threshold=.5*max(S)
-		#idxM=[j for j in idx if S[j]>threshold]
-		#print(scales[idxM])
-		#waitforEnter()
-	
-	averageS3=np.mean(allS,axis=0)
-	plt.plot(scales,averageS3,'g',lw=3)
+	scalogramAvgSpotify,scales = calcScalogram(spotifydown)
+	plt.plot(scales,scalogramAvgSpotify,'g',lw=3)
 	plt.show()
 	waitforEnter()
 
 	########### Profiling PornHub caps ###########
-	print("Profiling Spotify caps...")
-
-	allS=np.zeros((10,len(scales)))
-	for i in range(10):
-		S,scales=scalogram.scalogramCWT(pornhubdown[:,i],scales)
-		allS[i,:]=S
-		#plt.plot(scales,S,'r')
-		#plt.show()
-		#idx=argrelextrema(S, np.greater)[0]
-		#threshold=.5*max(S)
-		#idxM=[j for j in idx if S[j]>threshold]
-		#print(scales[idxM])
-		#waitforEnter()
-	
-	averageS4=np.mean(allS,axis=0)
-	plt.plot(scales,averageS4,'y',lw=3)
+	print("Profiling PornHub caps...")
+	scalogramAvgPornhub, scales = calcScalogram(pornhubdown)
+	plt.plot(scales,scalogramAvgPornhub,'y',lw=3)
 	plt.show()
 	waitforEnter()
 
-	
 	########### Performing the test ###########
 	print("Performing the test...")
 
-#	for i in range(1,20):
-	S,scales=scalogram.scalogramCWT(test[:],scales)
-	plt.plot(scales,S,'g')
+	N=300
+	dj=1/64
+	s0=2
+	J=1/dj * np.log2(0.5*N/s0)
+	scales=s0*2**(np.arange(J)*dj)
+
+	#for i in range(1,20):
+	S,scales=scalogram.scalogramCWT(test,scales)
+	plt.plot(scales,S,'m')
 	plt.show()
 	waitforEnter()
 
-	d = {"YouTube"  : curveDistance(averageS1,S),
-		 "Browsing" : curveDistance(averageS2,S),
-		 "Spotify"  : curveDistance(averageS3,S),
-		 "PornHub"	: curveDistance(averageS4,S)}
+	print('\nCurve Distances:')
 
-	print(min(d, key=d.get))
+	print("Youtube -> " + str(curveDistance(scalogramAvgYoutube, S)))
+	print("Browsing -> " + str(curveDistance(scalogramAvgBrowsing, S)))
+	print("Spotify -> " + str(curveDistance(scalogramAvgSpotify, S)))
+	print("PornHub -> " + str(curveDistance(scalogramAvgPornhub, S)))
+
+	d = {"YouTube"  : curveDistance(scalogramAvgYoutube,S),
+		 "Browsing" : curveDistance(scalogramAvgBrowsing,S),
+		 "Spotify"  : curveDistance(scalogramAvgSpotify,S),
+		 "PornHub"	: curveDistance(scalogramAvgPornhub,S)}
+
+	print("This is a " + str(min(d, key=d.get)) + " capture.")
 		
 	waitforEnter()
-#	plt.plot(scales,S,'b')
-#	plt.show()
 
 if __name__ == "__main__":
 	main()
